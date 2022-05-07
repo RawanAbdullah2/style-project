9bbcfe9d8dc4b14515e12193d2152f3ca204a7d1
RawanAbdullah2 <rawanroona20@gmail.com>
2022-05-08 01:05:28 +0300

    projectd

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -34,7 +34,7 @@
           Built by <a href="#" class="footer__link">Jonas Schmedtmann</a> for
           his online course
           <a href="#" class="footer__link">Advanced CSS and Sass</a>. Copyright
-          &copy; by Jonas Schmedtmann. You are 100% allowed to use this webpage
+          &copy; l Jonas Schmedtmann. You are 100% allowed to use this webpage
           for both personal and commercial use, but NOT to claim it as your own
           design. A credit to the original author, Jonas Schmedtmann, is of
           course highly appreciated!
